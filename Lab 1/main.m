36a7fb40d076d2bd5bc89907112c17474f497db7
SebasMac <ionrodriguezlara@gmail.com>
2023-08-28 23:19:50 -0700

    Fixed input receiving and added capitalizing

--- Lab 1/main.m
+++ Lab 1/main.m
@@ -15,19 +15,26 @@ int main(int argc, const char * argv[]) {
         BOOL needsInput = true;
         char inputChar[255];
         char choice[255];
+        NSString *inputString;
+        NSString *newString;
+        
         
         while (true) {
             if(needsInput) {
                 printf("Type your input:\n");
                 fgets(inputChar, 255, stdin);
+                inputString = [NSString stringWithUTF8String:inputChar];
                 needsInput = false;
             }
             //printf("Your string is %s\n", inputChar);
             printf("Operations:\n1. Uppercase\n2. Lowercase\n3. Numberize\n4. Canadianize\n5. Respond\n6. De-Space-It\n7. New Input\n");
             fgets(choice, 255, stdin);
-            int number = (int)choice;
+            int number = atoi(choice);
             if(number == 1) {
-                
+                //Capitalize
+                newString = inputString.localizedUppercaseString;
+                NSLog(@"Capitalized String: %@", newString);
+                continue;
             } else if(number == 2) {
                 
             } else if(number == 3) {
@@ -40,8 +47,10 @@ int main(int argc, const char * argv[]) {
                 
             } else if(number == 7) {
                 needsInput = true;
+                continue;
             } else {
-                
+                printf("Invalid Input");
+                continue;
             }
             
         }
